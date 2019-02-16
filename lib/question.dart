eda74ab677b633fe3dc3b2bce10e46ad4afab39b
misaelmateus <misaelmateus12@gmail.com>
2019-02-16 17:50:12 -0200

    Updated class question

--- lib/question.dart
+++ lib/question.dart
@@ -2,14 +2,18 @@ class QuestionData{
   String text;
   List<String> alternatives;
   int correctAlternative;
-
+  int chosed;
   QuestionData(String text, List<String> alternatives, int correctAlternative){
     this.text = text;
     this.alternatives = alternatives;
     this.correctAlternative = correctAlternative;
+    this.chosed = -1;
   }
 
   String getText() => this.text;
   List<String> getAlternatives() => alternatives;
   String textCorrectAlternative() => alternatives[correctAlternative];
+  void setChosed(int i){
+    this.chosed = i;
+  }
 }
--- lib/quizPage.dart
+++ lib/quizPage.dart
@@ -17,8 +17,6 @@ class quizPage extends StatefulWidget {
     list.add(new QuestionData('Quantas viagens são necessárias para o pirata levar todas as arcas para a ilha secreta?', ['1', '2', '3', '4', '5'], 2));
     list.add(new QuestionData('Quantas viagens são necessárias para o pirata levar todas as arcas para a ilha secreta?', ['1', '2', '3', '4', '5'], 2));
 
-
-
     data = new QuizData("Titulo", "Um pirata quer transportar, do seu navio para a sua ilha secreta, quatro arcas repletas de tesouros roubados: uma arca com Diamantes, uma arca com Esmeraldas, uma arca com Moedas de Ouro e uma arca com Moedas de Prata. Cada uma das arcas pesa 80 quilos, e o valor das arcas são diferentes entre si, sendo que a arca com Diamantes é a mais valiosa, seguida da arca com Esmeraldas, seguida da arca com Moedas de Ouro, seguida da arca com Moedas de Prata, que é a menos valiosa. O pirata tem apenas um barquinho para levar as arcas do navio para a ilha, e existem duas restrições:\n• o barquinho pode carregar, além do pirata, no máximo 200 quilos.\n• as arcas estão lacradas e não podem ser abertas; assim, o pirata deve levar a arca inteira no barquinho ou não levar a arca.\nNas questões abaixo, considere que uma viagem compreende o trajeto navio-ilha-navio.", list);
     return _quizPageState();
   }
@@ -27,6 +25,11 @@ class quizPage extends StatefulWidget {
 
 class _quizPageState extends State<quizPage> {
   QuizData questionData;
+
+  void _onButtonQuestionPressed(){
+
+  }
+
   @override
   Widget build(BuildContext context, ) {
     questionData = widget.data;
@@ -37,39 +40,48 @@ class _quizPageState extends State<quizPage> {
       ),
       body:
             ListView.builder(
-            itemCount: questionData.lenght(),
+            itemCount: questionData.lenght()+2,
             itemBuilder: (context, index) => Card(
               color: index == 0 ? Colors.cyan : Colors.white,
-              elevation: 1.0,
-              child: ExpansionTile(
-                title: Padding(
-                  padding: const EdgeInsets.all(18.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Text(
-                        index == 0 ? "Enunciado" : questionData.textQuestion(index-1),
-                        style: TextStyle(
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.bold,
-                        ),
+              elevation: index <= questionData.lenght() ? 1.0 : 0.0,
+              child:
+                index == questionData.lenght()+1
+                  ? Center(
+                      child:RaisedButton(
+                        child: Text("Avançar", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                        onPressed: (() =>_onButtonQuestionPressed()),
+                        color: Colors.blue,
+                      )
+                    )
+
+                  : ExpansionTile(
+                    title: Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Text(
+                            index == 0 ? "Enunciado" : questionData.textQuestion(index-1),
+                            style: TextStyle(
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.bold,
+                              fontStyle: FontStyle.normal
+                            ),
+                          ),
+                          ]
                       ),
-                      ]
+                    ),
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.grey[100],
+                      child: index == 0 ? Text("Q") : Text(String.fromCharCode(index + 48)),
+                    ),
+                    children: (index == 0 ?
+                        [StatementWidget(quizData: this.questionData)] :
+                        [AnswerWidget(questionData.getQuestion(index-1))] )
                   ),
-                ),
-                leading: CircleAvatar(
-                  backgroundColor: Colors.grey[100],
-                  child: index == 0 ? Text("Q") : Text(String.fromCharCode(index + 48)),
-                ),
-                children: (index == 0 ?
-                    [StatementWidget(quizData: this.questionData)] :
-                    questionData.getQuestion(index-1).getAlternatives().map((m) {
-                      return AnswerWidget(questionData.getQuestion(index-1), index-1, m, -1);
-                    }).toList()),
               ),
-            ),
-        )
+          )
     );
   }
 
@@ -91,34 +103,43 @@ class StatementWidget extends StatelessWidget {
 
 
 class AnswerWidget extends StatefulWidget {
-  final QuestionData results;
-  final int index;
-  final String m;
-  int marked;
-  AnswerWidget(this.results, this.index, this.m, this.marked);
+  final QuestionData questionData;
+  AnswerWidget(this.questionData);
 
   @override
   _AnswerWidgetState createState() => _AnswerWidgetState();
 }
 
 class _AnswerWidgetState extends State<AnswerWidget> {
-  Color c = Colors.black;
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      onTap: () {
-        setState(() {
-          c = Colors.blue;
-        });
-      },
-      title: Text(
-        widget.m,
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          color: c,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
+    List<Widget> tiles = new List<Widget>();
+    List<String> alternatives = widget.questionData.getAlternatives();
+    for(var i = 0; i < alternatives.length; i++){
+      print(alternatives[i]);
+      tiles.add(
+            ListTile(
+              onTap: () {
+                setState(() {
+                  widget.questionData.setChosed(i);
+                });
+              },
+              title: Text(
+                alternatives[i],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: i == widget.questionData.chosed ? Colors.blue : Colors.black,
+
+                ),
+              ),
+            )
+
+      );
+      if(i + 1 != alternatives.length)
+        tiles.add(Divider());
+    }
+    return Column(
+        children: tiles
     );
   }
 }
